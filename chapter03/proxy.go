ffc362ea3615dade9276efb0b779ba853dc2f013
KENN <cent.rngttwt@gmail.com>
2021-10-08 23:13:01 +0900

    [chapter03] add: proxy pattern

---
+++ chapter03/proxy.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+type HotelBoutiqueProxy struct {
+	subject *HotelBoutique
+}
+
+func (p *HotelBoutiqueProxy) Book() {
+	if p.subject == nil {
+		p.subject = new(HotelBoutique)
+	}
+	fmt.Println("Proxy Delegating Booking call")
+	p.subject.Book()
+}
+
+type HotelBoutique struct{}
+
+func (s *HotelBoutique) Book() {
+	fmt.Println("Booking done on external site")
+}
+
+func main() {
+	hb := new(HotelBoutique)
+	hb.Book()
+
+	hbp := new(HotelBoutiqueProxy)
+	hbp.Book()
+}
